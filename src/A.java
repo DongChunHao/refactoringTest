15995769f1d619fb2b473d26a8709db669867904
DongChunhao <m15231670380@163.com>
2023-06-02 19:18:10 +0800

    modify

--- src/A.java
+++ src/A.java
@@ -1,18 +1,8 @@
 public class A {
     public static Map<String, MetadataFieldMapper.TypeParser> getMetadataMappers(List<MapperPlugin> mapperPlugins) {
-        Map<String, MetadataFieldMapper.TypeParser> metadataMappers = new LinkedHashMap<>();
+        Map<String, MetadataFieldMapper.TypeParser> metadataMappers = new LinkedHashMap<>(builtInMetadataMappers);
 
-        int i = 0;
-        Map.Entry<String, MetadataFieldMapper.TypeParser> fieldNamesEntry = null;
-        for (Map.Entry<String, MetadataFieldMapper.TypeParser> entry : builtInMetadataMappers.entrySet()) {
-            if (i < builtInMetadataMappers.size() - 1) {
-                metadataMappers.put(entry.getKey(), entry.getValue());
-            } else {
-                assert entry.getKey().equals(FieldNamesFieldMapper.NAME) : "_field_names must be the last registered mapper, order counts";
-                fieldNamesEntry = entry;
-            }
-            i++;
-        }
+        Map.Entry<String, MetadataFieldMapper.TypeParser> fieldNamesEntry = metadataMappers.remove(FieldNamesFieldMapper.NAME);
         assert fieldNamesEntry != null;
 
         for (MapperPlugin mapperPlugin : mapperPlugins) {
@@ -20,14 +10,14 @@ public class A {
                 if (entry.getKey().equals(FieldNamesFieldMapper.NAME)) {
                     throw new IllegalArgumentException("Plugin cannot contain metadata mapper [" + FieldNamesFieldMapper.NAME + "]");
                 }
-                if (metadataMappers.put(entry.getKey(), entry.getValue()) != null) {
+                if (metadataMappers.putIfAbsent(entry.getKey(), entry.getValue()) != null) {
                     throw new IllegalArgumentException("MetadataFieldMapper [" + entry.getKey() + "] is already registered");
                 }
             }
         }
 
-        // we register _field_names here so that it has a chance to see all the other mappers, including from plugins
         metadataMappers.put(fieldNamesEntry.getKey(), fieldNamesEntry.getValue());
         return Collections.unmodifiableMap(metadataMappers);
     }
+
 }
