198a18aa346450bfaae8fe521c8ef982347641a2
DongChunhao <m15231670380@163.com>
2023-06-02 19:43:38 +0800

    modify

--- src/A.java
+++ src/A.java
@@ -1,19 +1,10 @@
 public class A {
     public static Map<String, MetadataFieldMapper.TypeParser> getMetadataMappers(List<MapperPlugin> mapperPlugins) {
-        Map<String, MetadataFieldMapper.TypeParser> metadataMappers = new LinkedHashMap<>();
-        int i = 0;
-        Map.Entry<String, MetadataFieldMapper.TypeParser> fieldNamesEntry = null;
-        for (Map.Entry<String, MetadataFieldMapper.TypeParser> entry : builtInMetadataMappers.entrySet()) {
-            if (i < builtInMetadataMappers.size() - 1) {
-                metadataMappers.put(entry.getKey(), entry.getValue());
-            } else {
-                assert entry.getKey().equals(FieldNamesFieldMapper.NAME) : "_field_names must be the last registered mapper, order counts";
-                fieldNamesEntry = entry;
-            }
-            i++;
-        }
+        Map<String, MetadataFieldMapper.TypeParser> metadataMappers = new LinkedHashMap<>(builtInMetadataMappers);
+
+        Map.Entry<String, MetadataFieldMapper.TypeParser> fieldNamesEntry = metadataMappers.remove(FieldNamesFieldMapper.NAME);
         ...
-                if (metadataMappers.put(entry.getKey(), entry.getValue()) != null) {
+                if (metadataMappers.putIfAbsent(entry.getKey(), entry.getValue()) != null) {
                     ...
                 }
 }
